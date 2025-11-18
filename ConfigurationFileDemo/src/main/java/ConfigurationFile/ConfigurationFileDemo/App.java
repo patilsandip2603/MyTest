b71ad7bb2f9b97162f089cbec8594d530c204401
Swami <Swami@DESKTOP-7QAV7JE>
2025-11-18 12:07:08 +0530

    Configuration File Demo Project - Committed

---
+++ ConfigurationFileDemo/src/main/java/ConfigurationFile/ConfigurationFileDemo/App.java
@@ -0,0 +1,13 @@
+package ConfigurationFile.ConfigurationFileDemo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ ConfigurationFileDemo/src/main/java/configFile/ConfigFileReader.java
@@ -0,0 +1,42 @@
+package configFile;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigFileReader 
+{
+	Properties prop;
+	File path = new File("C:\\Users\\Swami\\eclipse-workspace\\ConfigurationFileDemo\\src\\test\\java\\config.properties"); 
+	
+	ConfigFileReader() throws IOException
+	{
+		prop=new Properties();
+		FileInputStream fis = new FileInputStream(path);
+		prop.load(fis);
+		
+	}
+	
+	public String getUrl()
+	{
+		String url = prop.getProperty("url");
+		return url;
+		
+	}
+	public String username()
+	{
+		String username = prop.getProperty("username");
+		return username;
+		
+	}
+	public String password()
+	{
+		String password = prop.getProperty("password");
+		return password;
+		
+	}
+	
+	
+
+}
